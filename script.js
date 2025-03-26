99e7f3994b167fd26cebecb4de156ed682e189ef
codestutis <kestutisbiskis@gmail.com>
2025-03-26 11:44:13 -0400

    just some silly stuff

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById("dark-toggle").addEventListener('click', function() {
+let darkToggle = document.getElementById("dark-toggle");
+
+darkToggle.addEventListener('mouseover', function() {
+    const randomX = Math.random() * (window.innerWidth - darkToggle.offsetWidth);
+    const randomY = Math.random() * (window.innerHeight - darkToggle.offsetHeight);
+
+    darkToggle.style.position = 'absolute';
+    darkToggle.style.left = `${randomX}px`;
+    darkToggle.style.top = `${randomY}px`;
+})
+
+darkToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark');
 });
